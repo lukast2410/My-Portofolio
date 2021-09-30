19f1d5080e0fbe1d7b327c18b7f24106b8f9033e
lukast2410 <lt24k24@gmail.com>
2021-09-30 08:21:38 +0700

    Add Navbar Animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
 			<header className={styles.header}>
 				<div className="container">
 					<nav className={styles.nav}>
-						<h1 className={styles.logoNav}>L<span>T</span></h1>
+						<h1 className={styles.logoNav}>&lt;<span>L</span><span>T</span>&gt;</h1>
 						<ul>
 							<li><a href="#">Home</a></li>
 							<li><a href="#">About Me</a></li>

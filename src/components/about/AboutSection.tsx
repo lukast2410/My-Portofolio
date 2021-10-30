8adde7fd7462db162f42ba088e7b38323e4fdcf5
lukast2410 <lt24k24@gmail.com>
2021-10-30 15:06:44 +0700

    Add link at About and image with animation hover

--- src/components/about/AboutSection.tsx
+++ src/components/about/AboutSection.tsx
@@ -1,11 +1,14 @@
 import styles from '../../../styles/pages/About.module.scss'
 import AOS from 'aos'
 import { useEffect } from 'react'
+import { HiOutlineBriefcase } from 'react-icons/hi'
+import { FaBehanceSquare, FaGithubAlt, FaInstagram } from 'react-icons/fa'
+import { AiOutlineLinkedin } from 'react-icons/ai'
 
 export default function AboutSection() {
-  useEffect(() => {
-    AOS.init()
-  }, [])
+	useEffect(() => {
+		AOS.init()
+	}, [])
 
 	return (
 		<section className={styles.aboutSection} id='about'>
@@ -20,6 +23,10 @@ export default function AboutSection() {
 				<div className={styles.bubble}></div>
 			</div>
 
+			<div className={styles.images}>
+				<img src="/assets/me-bass.png" alt="Bass" />
+			</div>
+
 			<div className={styles.container}>
 				<h1
 					data-aos='fade-up'
@@ -31,13 +38,24 @@ export default function AboutSection() {
 					Design, Mobile Application Development, Frontend & Backend Development.
 				</p>
 				<p>
-					I always strive for excellent at building a functional application with aesthetic design to create
-					an amazing and great user experience.
+					I love to discover, learn, and make new things especially at building a functional application with
+					aesthetic design to create an amazing and great user experience. I also love to share my knowledge
+					and manage people to work with the best performance.
 				</p>
-				<p>
-					I love to discover, learn, and make new things. I also love to share my knowledge and manage people
-					to work with the best performance.
-				</p>
+				<div className={styles.links}>
+					<a href='https://github.com/lukast2410'>
+						<FaGithubAlt className={`icon6 ${styles.linkIcon}`} />
+					</a>
+					<a href='https://www.linkedin.com/in/lukas-t-b38884137/'>
+						<AiOutlineLinkedin className={`icon6 ${styles.linkIcon}`} />
+					</a>
+					<a href='https://www.instagram.com/lukast2410/'>
+						<FaInstagram className={`icon6 ${styles.linkIcon}`} />
+					</a>
+					<a href="https://www.behance.net/lukastanto">
+						<FaBehanceSquare className={`icon6 ${styles.linkIcon}`} />
+					</a>
+				</div>
 			</div>
 		</section>
 	)

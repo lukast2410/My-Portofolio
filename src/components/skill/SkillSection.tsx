94ee52e630d8461ed3b48c70db297bbd6645dfcb
lukast2410 <lt24k24@gmail.com>
2022-03-01 21:06:27 +0700

    Refactor Skill Code

--- src/components/skill/SkillSection.tsx
+++ src/components/skill/SkillSection.tsx
@@ -1,56 +1,31 @@
 import styles from '../../../styles/pages/Skill.module.scss'
-import { HiCode, HiOutlineDotsCircleHorizontal } from 'react-icons/hi'
-import { FaChevronDown, FaMobileAlt } from 'react-icons/fa'
-import { CgDatabase } from 'react-icons/cg'
-import { MdLanguage } from 'react-icons/md'
+import { FaChevronDown } from 'react-icons/fa'
 import { useEffect, useState } from 'react'
 import $ from 'jquery'
-import { AiOutlineApi } from 'react-icons/ai'
+import { skillsData } from '../../data/SkillData'
 
 export default function SkillSection() {
-	const [frontend, setFrondend] = useState(false)
-	const [backend, setBackend] = useState(false)
-	const [mobile, setMobile] = useState(false)
-	const [database, setDatabase] = useState(false)
-	const [programming, setProgramming] = useState(false)
-	const [other, setOther] = useState(false)
+	const [skillState, setSkillState] = useState([false, false, false, false, false, false])
 
-	const skillClicked = (skill: string) => {
-		if (skill == 'frontend') {
-			if (frontend) setFrondend(false)
-			else setFrondend(true)
-		} else if (skill == 'backend') {
-			if (backend) setBackend(false)
-			else setBackend(true)
-		} else if (skill == 'mobile') {
-			if (mobile) setMobile(false)
-			else setMobile(true)
-		} else if (skill == 'database') {
-			if (database) setDatabase(false)
-			else setDatabase(true)
-		} else if (skill == 'programming') {
-			if (programming) setProgramming(false)
-			else setProgramming(true)
-		} else if (skill == 'other') {
-			if (other) setOther(false)
-			else setOther(true)
+	const skillClicked = (skill: number) => {
+		if (skillState[skill]) {
+			const temp = skillState
+			temp[skill] = false
+			setSkillState([...temp])
+		} else {
+			const temp = skillState
+			temp[skill] = true
+			if(skill < 3){
+				temp[3] = false
+				temp[4] = false
+				temp[5] = false
+			}else{
+				temp[0] = false
+				temp[1] = false
+				temp[2] = false
+			}
+			setSkillState([...temp])
 		}
-	}
-
-	const skillBlured = (skill: string) => {
-		// if (skill == 'frontend' && frontend) {
-		// 	setFrondend(false)
-		// } else if (skill == 'backend' && backend) {
-		// 	setBackend(false)
-		// } else if (skill == 'mobile' && mobile) {
-		// 	setMobile(false)
-		// } else if (skill == 'programming' && programming) {
-		// 	setProgramming(false)
-		// } else if (skill == 'database' && database) {
-		// 	setDatabase(false)
-		// } else if (skill == 'other' && other) {
-		// 	setOther(false)
-		// }
 	}
 
 	useEffect(() => {
@@ -85,323 +60,39 @@ export default function SkillSection() {
 					<span>s</span>
 				</h1>
 				<div className={styles.skillsContainer}>
-					<div>
-						<div className={`${styles.skillGroup}  ${frontend ? styles.active : ''}`}>
-							<button className={`${styles.skillBtn}`} onClick={() => skillClicked('frontend')} onBlur={() => skillBlured('frontend')}>
-								<HiCode className='icon6' />
-								<span>Frontend</span>
-								<FaChevronDown className={`icon5 ${styles.chevron}`} />
-							</button>
-							<div className={`${styles.skillItems}`}>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/reactjs.png" alt="Skill" />
-									<span>React</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/angular.png" alt="Skill" />
-									<span>Angular</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/nextjs.png" alt="Skill" />
-									<span>NextJS</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/jquery.png" alt="Skill" />
-									<span>JQuery</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/tailwindcss.png" alt="Skill" />
-									<span>TailwindCSS</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/bootstrap.png" alt="Skill" />
-									<span>Bootstrap</span>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div>
-						<div className={`${styles.skillGroup}  ${backend ? styles.active : ''}`}>
-							<button className={styles.skillBtn} onClick={() => skillClicked('backend')} onBlur={() => skillBlured('backend')}>
-								<AiOutlineApi className='icon6' />
-								<span>Backend</span>
+					{skillsData.map((s, idx) => (
+						<div key={idx}>
+						<div className={`${styles.skillGroup} ${skillState[idx] ? styles.active : ''}`}>
+							<button
+								className={styles.skillBtn}
+								onClick={() => skillClicked(idx)}
+							>
+								<s.icon className='icon6' />
+								<span>{s.key}</span>
 								<FaChevronDown className={`icon5 ${styles.chevron}`} />
 							</button>
 							<div className={styles.skillItems}>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/laravel.png" alt="Skill" />
-									<span>Laravel</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/aspnet.png" alt="Skill" />
-									<span>ASP.NET</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/nestjs.png" alt="Skill" />
-									<span>NestJS</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/firebase.png" alt="Skill" />
-									<span>Firebase</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/golang.png" alt="Skill" />
-									<span>Go</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/php.png" alt="Skill" />
-									<span>PHP</span>
-								</div>
+								{s.skills.map((x, id) => (
+									<div key={id} className={styles.skillItem}>
+										<img src={x.src} alt='Skill' className={`${x.needBg ? styles.whiteBg : ''}`}/>
+										<span>{x.name}</span>
+									</div>
+								))}
 							</div>
 						</div>
 					</div>
-					<div>
-						<div className={`${styles.skillGroup}  ${mobile ? styles.active : ''}`}>
-							<button className={styles.skillBtn} onClick={() => skillClicked('mobile')} onBlur={() => skillBlured('mobile')}>
-								<FaMobileAlt className='icon6' />
-								<span>Mobile</span>
-								<FaChevronDown className={`icon5 ${styles.chevron}`} />
-							</button>
-							<div className={styles.skillItems}>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/flutter.svg" alt="Skill" />
-									<span>Flutter</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/swift.png" alt="Skill" />
-									<span>Swift</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/android_studio.png" alt="Skill" />
-									<span>Android Studio</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/java.png" alt="Skill" />
-									<span>Java</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/kotlin.png" alt="Skill" />
-									<span>Kotlin</span>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div>
-						<div className={`${styles.skillGroup}  ${database ? styles.active : ''}`}>
-							<button className={styles.skillBtn} onClick={() => skillClicked('database')} onBlur={() => skillBlured('database')}>
-								<CgDatabase className='icon6' />
-								<span>Database</span>
-								<FaChevronDown className={`icon5 ${styles.chevron}`} />
-							</button>
-							<div className={styles.skillItems}>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/postgresql.png" alt="Skill" />
-									<span>PostgreSQL</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/mysql.png" alt="Skill" className={styles.whiteBg} />
-									<span>MySQL</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/mariadb.png" alt="Skill" />
-									<span>MariaDB</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/sqlite.png" alt="Skill" />
-									<span>SQLite</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/mongodb.png" alt="Skill" />
-									<span>MongoDB</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/sql_server.png" alt="Skill" />
-									<span>SQL Server</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/firebase-realtimedb.png" alt="Skill" />
-									<span>Firebase Realtime Database</span>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div>
-						<div className={`${styles.skillGroup}  ${programming ? styles.active : ''}`}>
-							<button className={styles.skillBtn} onClick={() => skillClicked('programming')} onBlur={() => skillBlured('programming')}>
-								<MdLanguage className='icon6' />
-								<span>Programming</span>
-								<FaChevronDown className={`icon5 ${styles.chevron}`} />
-							</button>
-							<div className={styles.skillItems}>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/c.png" alt="Skill" />
-									<span>C</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/csharp.png" alt="Skill" />
-									<span>C#</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/js.png" alt="Skill" />
-									<span>Javascript</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/ts.png" alt="Skill" />
-									<span>Typescript</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/python.png" alt="Skill" />
-									<span>Python</span>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div>
-						<div className={`${styles.skillGroup}  ${other ? styles.active : ''}`}>
-							<button className={styles.skillBtn} onClick={() => skillClicked('other')} onBlur={() => skillBlured('other')}>
-								<HiOutlineDotsCircleHorizontal className='icon6' />
-								<span>Other</span>
-								<FaChevronDown className={`icon5 ${styles.chevron}`} />
-							</button>
-							<div className={styles.skillItems}>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/unity.png" alt="Skill" />
-									<span>Unity</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/premierepro.png" alt="Skill" />
-									<span>Premiere Pro</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/aftereffect.png" alt="Skill" />
-									<span>After Effect</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/photoshop.png" alt="Skill" />
-									<span>Photoshop</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/graphql.png" alt="Skill" />
-									<span>Graphql</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/axios.png" alt="Skill" className={styles.whiteBg}/>
-									<span>Axios</span>
-								</div>
-								<div className={styles.skillItem}>
-									<img src="/assets/skill/figma.png" alt="Skill" />
-									<span>Figma</span>
-								</div>
-							</div>
-						</div>
-					</div>
+					))}
 				</div>
-				{/* <div className={styles.skills}>
-					<div className={styles.skill5}>
-						<div className={`${styles.skillItem} effect`} data-value={2}>
-							<img src='/assets/skill/reactjs.png' alt='ReactJS' />
-							<h4>ReactJS</h4>
-						</div>
-						<div className={`${styles.skillItem} effect`} data-value={-2}>
-							<img src='/assets/skill/angular.png' alt='AngularJS' />
-							<h4>AngularJS</h4>
-						</div>
-						<div className={`${styles.skillItem} effect`} data-value={4}>
-							<img src='/assets/skill/nextjs.png' alt='NextJS' />
-							<h4>NextJS</h4>
-						</div>
-						<div className={`${styles.skillItem} effect`} data-value={3}>
-							<img src='/assets/skill/nestjs.png' alt='NestJS' />
-							<h4>NestJS</h4>
-						</div>
-						<div className={`${styles.skillItem} effect`} data-value={-4}>
-							<img src='/assets/skill/golang.png' alt='Golang' />
-							<h4>Go</h4>
-						</div>
-					</div>
-					<div className={styles.skill4}>
-						<div className={`${styles.skillItem} effect`} data-value={-2}>
-							<img src='/assets/skill/laravel.png' alt='Laravel' />
-							<h4>Laravel</h4>
-						</div>
-						<div className={`${styles.skillItem} effect`} data-value={3}>
-							<img src='/assets/skill/sql_server.png' alt='SQL Server' />
-							<h4>SQL Server</h4>
-						</div>
-						<div className={`${styles.skillItem} effect`} data-value={4}>
-							<img src='/assets/skill/firebase.png' alt='Firebase' />
-							<h4>Firebase</h4>
-						</div>
-						<div className={`${styles.skillItem} effect`} data-value={-1}>
-							<img src='/assets/skill/unity.png' alt='Unity' />
-							<h4>Unity</h4>
-						</div>
-					</div>
-					<div className={styles.skill3}>
-						<div className={`${styles.skillItem} effect`} data-value={2}>
-							<img src='/assets/skill/android_studio.png' alt='Android' />
-							<h4>Android Studio</h4>
-						</div>
-						<div className={`${styles.skillItem} effect`} data-value={-1}>
-							<img src='/assets/skill/flutter.svg' alt='Flutter' />
-							<h4>Flutter</h4>
-						</div>
-						<div className={`${styles.skillItem} effect`} data-value={3}>
-							<img src='/assets/skill/ios.png' alt='iOS' />
-							<h4>iOS</h4>
-						</div>
-					</div>
-					<div className={styles.skill2}>
-						<div className={`${styles.skillItem} effect`} data-value={-1}>
-							<img src='/assets/skill/photoshop.png' alt='Photoshop' />
-							<h4>Photoshop</h4>
-						</div>
-						<div className={`${styles.skillItem} effect`} data-value={-3}>
-							<img src='/assets/skill/premierepro.png' alt='PremierePro' />
-							<h4>Premiere Pro</h4>
-						</div>
-					</div>
-				</div> */}
 			</div>
-			{/* <div className={styles.slideContainer}>
-				<div className={styles.skillSliding}>
-					<span>HTML</span>
-					<span>CSS</span>
-					<span>JS</span>
-					<span>TS</span>
-					<span>PHP</span>
-					<span>T-SQL</span>
-					<span>PostgreSQL</span>
-					<span>Swift</span>
-					<span>HTML</span>
-					<span>CSS</span>
-					<span>JS</span>
-					<span>TS</span>
-				</div>
-				<div className={styles.skillSliding}>
-					<span>Dart</span>
-					<span>Kotlin</span>
-					<span>Java</span>
-					<span>C#</span>
-					<span>C</span>
-					<span>Python</span>
-					<span>TailwindCSS</span>
-					<span>Graphql</span>
-					<span>Dart</span>
-					<span>Kotlin</span>
-					<span>Java</span>
-					<span>C#</span>
-				</div>
-			</div> */}
 			<div className={styles.fishContainer}>
-				<img src="/assets/fish/1.gif" alt="Fish" />
-				<img src="/assets/fish/2.gif" alt="Fish" />
-				<img src="/assets/fish/3.gif" alt="Fish" />
-				<img src="/assets/fish/4.gif" alt="Fish" />
-				<img src="/assets/fish/2.gif" alt="Fish" />
-				<img src="/assets/fish/3.gif" alt="Fish" />
-				<img src="/assets/fish/4.gif" alt="Fish" />
-				<img src="/assets/fish/1.gif" alt="Fish" />
+				<img src='/assets/fish/1.gif' alt='Fish' />
+				<img src='/assets/fish/2.gif' alt='Fish' />
+				<img src='/assets/fish/3.gif' alt='Fish' />
+				<img src='/assets/fish/4.gif' alt='Fish' />
+				<img src='/assets/fish/2.gif' alt='Fish' />
+				<img src='/assets/fish/3.gif' alt='Fish' />
+				<img src='/assets/fish/4.gif' alt='Fish' />
+				<img src='/assets/fish/1.gif' alt='Fish' />
 			</div>
 		</section>
 	)
---
+++ src/data/SkillData.ts
@@ -0,0 +1,224 @@
+import { AiOutlineApi } from "react-icons/ai";
+import { CgDatabase } from "react-icons/cg";
+import { FaMobileAlt } from "react-icons/fa";
+import { HiCode, HiOutlineDotsCircleHorizontal } from "react-icons/hi";
+import { MdLanguage } from "react-icons/md";
+
+export const skillsData = [
+	{
+		key: 'frontend',
+    icon: HiCode,
+    skills: [
+      {
+        src: '/assets/skill/reactjs.png',
+        needBg: false,
+        name: 'React'
+      },
+      {
+        src: '/assets/skill/angular.png',
+        needBg: false,
+        name: 'Angular'
+      },
+      {
+        src: '/assets/skill/nextjs.png',
+        needBg: false,
+        name: 'NextJS'
+      },
+      {
+        src: '/assets/skill/jquery.png',
+        needBg: false,
+        name: 'JQuery'
+      },
+      {
+        src: '/assets/skill/tailwindcss.png',
+        needBg: false,
+        name: 'TailwindCSS'
+      },
+      {
+        src: '/assets/skill/bootstrap.png',
+        needBg: false,
+        name: 'Bootstrap'
+      },
+    ]
+	},
+	{
+		key: 'backend',
+    icon: AiOutlineApi,
+    skills: [
+      {
+        src: '/assets/skill/laravel.png',
+        needBg: false,
+        name: 'Laravel'
+      },
+      {
+        src: '/assets/skill/aspnet.png',
+        needBg: false,
+        name: 'ASP.NET'
+      },
+      {
+        src: '/assets/skill/nestjs.png',
+        needBg: false,
+        name: 'NestJS'
+      },
+      {
+        src: '/assets/skill/firebase.png',
+        needBg: false,
+        name: 'Firebase'
+      },
+      {
+        src: '/assets/skill/golang.png',
+        needBg: false,
+        name: 'Go'
+      },
+      {
+        src: '/assets/skill/php.png',
+        needBg: false,
+        name: 'PHP'
+      },
+    ]
+	},
+	{
+		key: 'mobile',
+    icon: FaMobileAlt,
+    skills: [
+      {
+        src: '/assets/skill/flutter.svg',
+        needBg: false,
+        name: 'Flutter'
+      },
+      {
+        src: '/assets/skill/swift.png',
+        needBg: false,
+        name: 'Swift'
+      },
+      {
+        src: '/assets/skill/android_studio.png',
+        needBg: false,
+        name: 'Android Studio'
+      },
+      {
+        src: '/assets/skill/java.png',
+        needBg: false,
+        name: 'Java'
+      },
+      {
+        src: '/assets/skill/kotlin.png',
+        needBg: false,
+        name: 'Kotlin'
+      },
+    ]
+	},
+	{
+		key: 'database',
+    icon: CgDatabase,
+    skills: [
+      {
+        src: '/assets/skill/postgresql.png',
+        needBg: false,
+        name: 'PostgreSQL'
+      },
+      {
+        src: '/assets/skill/mysql.png',
+        needBg: true,
+        name: 'MySQL'
+      },
+      {
+        src: '/assets/skill/mariadb.png',
+        needBg: false,
+        name: 'MariaDB'
+      },
+      {
+        src: '/assets/skill/sqlite.png',
+        needBg: false,
+        name: 'SQLite'
+      },
+      {
+        src: '/assets/skill/mongodb.png',
+        needBg: false,
+        name: 'MongoDB'
+      },
+      {
+        src: '/assets/skill/sql_server.png',
+        needBg: false,
+        name: 'SQL Server'
+      },
+      {
+        src: '/assets/skill/firebase-realtimedb.png',
+        needBg: false,
+        name: 'Firebase Realtime Database'
+      },
+    ]
+	},
+	{
+		key: 'programming',
+    icon: MdLanguage,
+    skills: [
+      {
+        src: '/assets/skill/c.png',
+        needBg: false,
+        name: 'C'
+      },
+      {
+        src: '/assets/skill/csharp.png',
+        needBg: false,
+        name: 'C#'
+      },
+      {
+        src: '/assets/skill/js.png',
+        needBg: false,
+        name: 'Javascript'
+      },
+      {
+        src: '/assets/skill/ts.png',
+        needBg: false,
+        name: 'Typescript'
+      },
+      {
+        src: '/assets/skill/python.png',
+        needBg: false,
+        name: 'Python'
+      },
+    ]
+	},
+	{
+		key: 'others',
+    icon: HiOutlineDotsCircleHorizontal,
+    skills: [
+      {
+        src: '/assets/skill/unity.png',
+        needBg: false,
+        name: 'Unity'
+      },
+      {
+        src: '/assets/skill/premierepro.png',
+        needBg: false,
+        name: 'Premiere Pro'
+      },
+      {
+        src: '/assets/skill/aftereffect.png',
+        needBg: false,
+        name: 'After Effect'
+      },
+      {
+        src: '/assets/skill/photoshop.png',
+        needBg: false,
+        name: 'Photoshop'
+      },
+      {
+        src: '/assets/skill/graphql.png',
+        needBg: false,
+        name: 'Graphql'
+      },
+      {
+        src: '/assets/skill/axios.png',
+        needBg: true,
+        name: 'Axios'
+      },
+      {
+        src: '/assets/skill/figma.png',
+        needBg: false,
+        name: 'Figma'
+      },
+    ]
+	},
+]
